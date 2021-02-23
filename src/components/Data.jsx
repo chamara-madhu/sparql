d08a4d815c617e7782e8fb15ad13e141d33f0b5c
Chamara <chamara@spritzer.app>
2021-02-23 21:39:36 +0530

    filtering

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -4,23 +4,23 @@ import qs from "qs";
 
 export default class Terms extends Component {
   state = {
+    zipcode: "",
+    name: "",
+    policedistrict: "",
+    neighborhood: "",
+
     data: [],
-    params: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
   componentDidMount = () => {
-    const data = qs.stringify({
-      query: `select * where { ?s ?p ?o }`,
-    });
-
     axios
-      .post("http://sparql.cancerdata.org/namespace/undefined/sparql", data, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded;",
-        },
-      })
+      .get("http://localhost:9090/getAllItems")
       .then((res) => {
-        this.setState({ data: res.data.results.bindings });
+        this.setState({ data: res.data });
       })
       .catch((err) => {
         console.log(err);
@@ -30,19 +30,31 @@ export default class Terms extends Component {
   handleSearch = (e) => {
     e.preventDefault();
 
-    if (this.state.params) {
-      const data = qs.stringify({
-        query: `select ${this.state.params} where { ?s ?p ?o }`,
-      });
+    if (
+      this.state.zipcode ||
+      this.state.name ||
+      this.state.policedistrict ||
+      this.state.neighborhood
+    ) {
+      const data = {};
+
+      if (this.state.zipcode) {
+        data.zipcode = this.state.zipcode;
+      }
+      if (this.state.name) {
+        data.name = this.state.zipcode;
+      }
+      if (this.state.policedistrict) {
+        data.policedistrict = this.state.zipcode;
+      }
+      if (this.state.neighborhood) {
+        data.neighborhood = this.state.zipcode;
+      }
 
       axios
-        .post("http://sparql.cancerdata.org/namespace/undefined/sparql", data, {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded;",
-          },
-        })
+        .post("http://localhost:9090/findPlace", data)
         .then((res) => {
-          this.setState({ data: res.data.results.bindings });
+          this.setState({ data: res.data });
         })
         .catch((err) => {
           console.log(err);
@@ -52,45 +64,82 @@ export default class Terms extends Component {
 
   render() {
     return (
-      <div className="row m-0">
-        <div className="col p-0">
-          <form class="form-inline">
-            <label class="sr-only" for="inlineFormInputName2">
-              Name
-            </label>
-            <input
-              type="text"
-              class="form-control mb-2 mr-sm-2"
-              id="inlineFormInputName2"
-              placeholder="Params"
-              value={this.state.params}
-              onChange={(e) => this.setState({ params: e.target.value })}
-            />
+      <div className="container mt-5">
+        <div className="row m-0">
+          <div className="col p-0">
+            <form className="mb-5">
+              <div className="form-row">
+                <div className="form-group col-md-3">
+                  <label htmlFor="zipcode">ZIP Code</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="zipcode"
+                    name="zipcode"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group col-md-3">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group col-md-3">
+                  <label htmlFor="policedistrict">Police District</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="policedistrict"
+                    name="policedistrict"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group col-md-3">
+                  <label htmlFor="neighborhood">Neighborhood</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="neighborhood"
+                    name="neighborhood"
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
 
-            <button
-              type="submit"
-              class="btn btn-primary mb-2"
-              onClick={this.handleSearch}
-            >
-              Search
-            </button>
-          </form>
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>O</td>
-                <td>P</td>
-                <td>S</td>
-              </tr>
-              {this.state.data.map((el, i) => (
-                <tr key={i}>
-                  {el.o && <td>{el.o.value}</td>}
-                  {el.p && <td>{el.p.value}</td>}
-                  {el.s && <td>{el.s.value}</td>}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+              <button
+                type="submit"
+                className="btn btn-primary d-block m-auto"
+                onClick={this.handleSearch}
+              >
+                Search
+              </button>
+            </form>
+            {this.state.data.length > 0 && (
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <td>Zip Code</td>
+                    <td>Name</td>
+                    <td>Police District</td>
+                    <td>Neighborhood</td>
+                  </tr>
+                  {this.state.data.map((el, i) => (
+                    <tr key={i}>
+                      <td>{el.zipcode}</td>
+                      <td>{el.name}</td>
+                      <td>{el.policedistrict}</td>
+                      <td>{el.neighborhood}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
         </div>
       </div>
     );
